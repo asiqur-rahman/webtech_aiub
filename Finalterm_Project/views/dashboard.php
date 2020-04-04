e280158056eaeac556accea2450b22ee35234f90
shokaljoy <shokal.joy51@users.noreply.github.com>
2020-04-04 21:34:55 +0600

    Create dashboard.php

---
+++ Finalterm_Project/views/dashboard.php
@@ -0,0 +1,38 @@
+<?php
+include 'main_header.php';
+$usertype=$_GET["user"];
+if($usertype=="admin")
+{
+<<<<<<< HEAD
+<<<<<<< HEAD
+    
+=======
+  echo "Admin";
+>>>>>>> 9d1e7fcaae43bdbd5f35e34c945700f037bfa9c2
+=======
+  echo "Admin";
+>>>>>>> 9d1e7fcaae43bdbd5f35e34c945700f037bfa9c2
+}
+else if($usertype=="student")
+{
+  echo "Student";
+}
+else if($usertype=="superAdmin")
+{
+  echo "SuperAdmin";
+}
+else if($usertype=="teacher")
+{
+<<<<<<< HEAD
+<<<<<<< HEAD
+    
+=======
+  echo "Teacher";
+>>>>>>> 9d1e7fcaae43bdbd5f35e34c945700f037bfa9c2
+=======
+  echo "Teacher";
+>>>>>>> 9d1e7fcaae43bdbd5f35e34c945700f037bfa9c2
+}
+
+include 'main_footer.php';
+?>
