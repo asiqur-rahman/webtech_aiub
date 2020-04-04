4b47fb5baf07201196946ed490ab660f8d7912d4
asiqur-rahman <md.ashik7061@gmail.com>
2020-04-04 19:38:28 +0600

    few change happen

--- Finalterm_Project/views/dashboard.php
+++ Finalterm_Project/views/dashboard.php
@@ -3,19 +3,19 @@ include 'main_header.php';
 $usertype=$_GET["user"];
 if($usertype=="admin")
 {
-
+  echo "Admin";
 }
 else if($usertype=="student")
 {
-
+  echo "Student";
 }
 else if($usertype=="superAdmin")
 {
-
+  echo "SuperAdmin";
 }
 else if($usertype=="teacher")
 {
-
+  echo "Teacher";
 }
 
 include 'main_footer.php';
