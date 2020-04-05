20a44ceaf3c282d0e3670911528f2ec3cffda1b9
asiqur-rahman <md.ashik7061@gmail.com>
2020-04-05 09:50:38 +0600

    a little update

--- Finalterm_Project/views/login.php
+++ Finalterm_Project/views/login.php
@@ -16,20 +16,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<title>Login</title>
 		<script>
-			function keypress()
-			{
-				document.getElementById('error').innerHTML="";
-				var uname=document.getElementById('uname').value;
-				var upass=document.getElementById('upass').value;
-					var btn=document.getElementById('submit');
-					const button = document.getElementById('btn');
-					if(uname!="" && upass!=""){
-						btn.style.display="inline";
-					}
-					else {
-						btn.style.display="none";
-					}
-			}
+
 			function signup()
 			{
 				alert("Unfortunately! Sign up in unavailable ");
@@ -58,7 +45,7 @@
 				<?php echo $error; ?>
 			</div>
 			<div id="btn">
-				<input type="submit" id="submit" name="login-submit" class="btn btn-danger" value="Login" class="form-control" style="display:none; width:100%;">
+				<input type="submit" id="submit" name="login-submit" class="btn btn-danger" value="Login" class="form-control" style=" width:100%;">
 			</div>
 		</div>
 	</form>
